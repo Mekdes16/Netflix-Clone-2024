8304450d2d13fef69131f6a927643af66e7f33ff
Mekdes16 <mekdesk14@gmail.com>
2024-09-13 18:48:40 -0700

    update banner & row css

--- netflix-clone/src/Components/Rows/Row/Row.jsx
+++ netflix-clone/src/Components/Rows/Row/Row.jsx
@@ -58,7 +58,7 @@ const Row = ({ title, fetchUrl, isLargeRow }) => {
                     />
                 ))}
             </div>
-            <div style={{ padding: '40px' }}>
+            <div style={{ padding: '15px' }}>
                 {trailerUrl && <YouTube videoId={trailerUrl} opts={opts} />}
             </div>
         </div>
